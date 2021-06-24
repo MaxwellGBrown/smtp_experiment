36029ca51f311db6f80faad62d319cb37531189e
Maxwell Brown <maxwellgbrown@gmail.com>
2021-06-23 22:49:14 -0400

    Implement simple mail mail transaction flow
    https://datatracker.ietf.org/doc/html/rfc5321#section-3.3

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 """Send messages to SMTP server."""
+import email.message
 import smtplib
 import sys
 
@@ -6,9 +7,15 @@ import sys
 def main(port):
     """Run an SMTP client to server."""
     with smtplib.SMTP(host="localhost", port=port) as smtp:
-        smtp.noop()
-        smtp.noop()
-        smtp.noop()
+        smtp.set_debuglevel(1)
+
+        message = email.message.EmailMessage()
+        message.set_content("Hello World")
+        message["Subject"] = "Hello World"
+        message["From"] = "foo@test.com"
+        message["To"] = "bar@test.com"
+
+        smtp.send_message(message)
         smtp.quit()
 
 
--- server.py
+++ server.py
@@ -1,25 +1,13 @@
-"""SMTP server implementation."""
+"""SMTP server implementation.
+
+SMTP RFC
+https://datatracker.ietf.org/doc/html/rfc5321
+
+SMTP Status Codes & Responses
+https://datatracker.ietf.org/doc/html/rfc3463
+"""
 import socket
 import sys
-
-
-class Command:
-    """Represents an SMTP command."""
-
-    def __init__(self, raw):
-        """Turn raw message into readable message."""
-        self.command, *rest = raw.split(b" ")
-        self.data = b" ".join(rest).rstrip(b"\r\n")
-
-
-def receive(connection):
-    """Receive command from connection."""
-    message = b""
-    while not message.endswith(b"\r\n"):
-        message += connection.recv(1024)
-
-    print("Received message: {}".format(message.decode().rstrip("\r\n")))
-    return Command(message)
 
 
 def main(host="localhost", port=8888):
@@ -38,10 +26,62 @@ def main(host="localhost", port=8888):
                 # Greet the connection
                 connection.send(b"220 OK\r\n")
 
-                while (command := receive(connection)).command != b"QUIT":
-                    connection.send(b"220 OK\r\n")
+                while True:
+                    message = b""
+                    while not message.endswith(b"\r\n"):
+                        message += connection.recv(1024)
 
-            print("Connection closed!")
+                    command, *rest = message.split(b" ")
+                    if command == b"QUIT":
+                        break
+                    elif command.upper() == b"MAIL":
+                        # Requires a HELO or EHLO first, with "220 OK" response
+
+                        # Begin a Mail Transaction
+                        # https://datatracker.ietf.org/doc/html/rfc5321#section-3.3
+
+                        # 1. MAIL FROM:<sender>
+                        #    250 OK -- If sender is accepted
+                        #    550 or 553 if failed (TODO)
+                        print(message)
+                        connection.send(b"250 OK\r\n")
+                        print(b">>> 250 OK\r\n")
+
+                        # 2. RCPT TO:<recievers>
+                        #    250 OK
+                        reciept = b""
+                        while not reciept.endswith(b"\r\n"):
+                            reciept += connection.recv(1024)
+                        print(reciept)
+                        connection.send(b"250 OK\r\n")
+                        print(b">>> 250 OK\r\n")
+
+                        # 3. DATA\r\n
+                        #    354 Intermediate
+                        #
+                        #    Fails if there was no MAIL or RCPT command
+                        #    Return "503 Command out of sequence" or "5"
+                        start_email = b""
+                        while not start_email.endswith(b"\r\n"):
+                            start_email += connection.recv(1024)
+                        print(start_email)
+                        connection.send(b"354 Intermediate\r\n")
+                        print(b">>> 354 Intermediate\r\n")
+
+                        # 4. All lines up until a line with a single period
+                        #    are considered a part of the message data.
+                        #    (e.g. "Hello World\r\n.\r\n")
+                        email_data = b""
+                        while not email_data.endswith(b"\r\n.\r\n"):
+                            email_data += connection.recv(1024)
+                        print(email_data)
+                        connection.send(b"250 OK\r\n")
+                        print(b">>> 250 OK\r\n")
+                    else:
+                        print(message)
+                        connection.send(b"220 OK\r\n")
+
+        print("Connection closed!")
 
 
 if __name__ == "__main__":
